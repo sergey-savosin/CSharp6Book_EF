0b4cbfd2e46121c5f64f6e868b15810d1ff3bb05
SavosinSergey <SavosinSergey@SERGEYPC>
2017-01-26 22:23:28 +0300

    Добавьте файлы проекта.

---
+++ AutoLotDAL/EF/AutoLotEntities.cs
@@ -0,0 +1,25 @@
+﻿namespace AutoLotDAL.EF
+{
+    using AutoLotDAL.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class AutoLotEntities : DbContext
+    {
+        public AutoLotEntities()
+            : base("name=AutoLotConnection")
+        {
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<CreditRisk> CreditRisks { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Inventory> Inventory { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+    }
+
+}
---
+++ AutoLotDAL/Models/Partials/InventoryPartial.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutoLotDAL.Models
+{
+    public partial class Inventory
+    {
+        public override string ToString()
+        {
+            return $"{this.PetName ?? "** No Name **"} is a {this.Color} {this.Make} with ID {this.CarId}.";
+        }
+
+        [NotMapped]
+        public string MakeColor => $"{Make} + ({Color})";
+    }
+}
