7f5a149d263c9a095cf2e80176f67dfe343033a1
SavosinSergey <SavosinSergey@SERGEYPC>
2017-01-26 23:25:27 +0300

    Репозиторий

---
+++ AutoLotDAL/Repos/BaseRepo.cs
@@ -0,0 +1,139 @@
+﻿using AutoLotDAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLotDAL.Repos
+{
+    public abstract class BaseRepo<T>: IDisposable where T: class, new()
+    {
+        public AutoLotEntities Context { get; } = new AutoLotEntities();
+        protected DbSet<T> Table;
+
+        internal int SaveChanges()
+        {
+            try
+            {
+                return Context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw;
+            }
+            catch (CommitFailedException ex)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        internal async Task<int> SaveChangesAsync()
+        {
+            try
+            {
+                return await Context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw;
+            }
+            catch (DbUpdateException ex)
+            {
+                throw;
+            }
+            catch (CommitFailedException ex)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public int Save(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            return SaveChanges();
+        }
+        public Task<int> SaveAsync(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            return SaveChangesAsync();
+        }
+
+        public T GetOne(int? id) => Table.Find(id);
+        public Task<T> GetOneAsync(int? id) => Table.FindAsync(id);
+        public List<T> GetAll() => Table.ToList();
+        public Task<List<T>> GetAllAsync() => Table.ToListAsync();
+
+        public List<T> ExecuteQuery(string sql) => Table.SqlQuery(sql).ToList();
+        public Task<List<T>> ExecuteQueryAsync(string sql) => Table.SqlQuery(sql).ToListAsync();
+        public List<T> ExecuteQuery(string sql, object[] sqlParametersObjects)
+                => Table.SqlQuery(sql, sqlParametersObjects).ToList();
+        public Task<List<T>> ExecuteQueryAsync(string sql, object[] sqlParametersObjects)
+                => Table.SqlQuery(sql, sqlParametersObjects).ToListAsync();
+
+        public int Add(T entity)
+        {
+            Table.Add(entity);
+            return SaveChanges();
+        }
+        public Task<int> AddAsync(T entity)
+        {
+            Table.Add(entity);
+            return SaveChangesAsync();
+        }
+        public int AddRange(IList<T> entities)
+        {
+            Table.AddRange(entities);
+            return SaveChanges();
+        }
+        public Task<int> AddRangeAsync(IList<T> entities)
+        {
+            Table.AddRange(entities);
+            return SaveChangesAsync();
+        }
+
+        public int Delete (T entity)
+        {
+            Context.Entry(entity).State = EntityState.Deleted;
+            return SaveChanges();
+        }
+        public Task<int> DeleteAsync (T entity)
+        {
+            Context.Entry(entity).State = EntityState.Deleted;
+            return SaveChangesAsync();
+        }
+
+        bool disposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+            if (disposing)
+            {
+                Context.Dispose();
+                // Free any managed objects here
+                //
+            }
+
+            // Free any unmanaged objects here
+            //
+            disposed = true;
+        }
+    }
+
+}
---
+++ AutoLotDAL/Repos/IRepo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLotDAL.Repos
+{
+    interface IRepo<T>
+    {
+        int Add(T entity);
+        Task<int> AddAsync(T entity);
+
+        int AddRange(IList<T> entities);
+        Task<int> AddRangeAsync(IList<T> entities);
+
+        int Save(T entity);
+        Task<int> SaveAsync(T entity);
+
+        int Delete(T entity);
+        Task<int> DeleteAsync(T entity);
+
+        T GetOne(int? id);
+        Task<T> GetOneAsync(int? id);
+
+        List<T> GetAll();
+        Task<List<T>> GetAllAsync();
+
+        List<T> ExecuteQuery(string sql);
+        Task<List<T>> ExecuteQueryAsync(string sql);
+
+        List<T> ExecuteQuery(string sql, object[] sqlParametersObjects);
+        Task<List<T>> ExecuteQueryAsync(string sql, object[] sqlParametersObjects);
+    }
+}
