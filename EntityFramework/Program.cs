76f23c79b96adf37d89cd3d330e7eb50249f90ce
SavosinSergey <SavosinSergey@SERGEYPC>
2017-01-28 19:32:37 +0300

    DataInitializer. Sample app.

--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using AutoLotDAL.EF;
+using AutoLotDAL.Models;
+using AutoLotDAL.Repos;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +14,21 @@ namespace EntityFramework
     {
         static void Main(string[] args)
         {
+            Database.SetInitializer(new DataInitializer());
+            Console.WriteLine("**** Fun with ADO.NET EF Code First ****\n");
+            PrintAllInventory();
+            Console.ReadLine();
+        }
+
+        private static void PrintAllInventory()
+        {
+            using (var repo = new InventoryRepo())
+            {
+                foreach(Inventory c in repo.GetAll())
+                {
+                    Console.WriteLine(c);
+                }
+            }
         }
     }
 }
